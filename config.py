daaeb46b33ba1218fdb7fc6845fb0ee89cb34df4
justinbellavance <justin.bellavance@gmail.com>
2024-06-19 22:22:43 +0000

    small changes to sample size, case controls

---
+++ config.py
@@ -0,0 +1,18 @@
+hg_build_number = 38
+
+pval_is_neglog10 = True
+
+field_aliases = {
+        "CHROM" : "chrom",
+        "GENPOS" : "pos",
+        "ALLELE0" : "ref",
+        "ALLELE1" : "alt",
+        "A1FREQ" : "af",
+        "BETA" : "beta",
+        "SE" : "sebeta",
+        "LOG10P" : "pval"
+}
+
+data_dir = "/mnt/s3/CLSA_PheWeb_r1"
+
+show_manhattan_filter_button=True
